14cf0013b884f423d20b5479c5bcbe36af68f64e
xmomoex <moimorex@gmail.com>
2024-06-18 18:22:00 +0200

    css

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,16 @@ Route::get('/email/verify', function () {
     return view('auth.verify-email');
 })->middleware('auth')->name('auth.verify-email');
 
-
+/*
 Route::get('/', function () {
     return redirect()->route('login');
 });
-
-/*
-Route::get('/', function () {
-    return view('welcome');
-});
 */
+
+
+
+
+
 
 Route::resource('challenges', ChallengeController::class);
 
@@ -33,6 +33,7 @@ Route::resource('challenges', ChallengeController::class);
 //Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/',  [PostController::class, 'index'])->name('posts.index');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
